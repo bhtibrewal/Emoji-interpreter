a6a29f9916845928f2c6be7cfd9254566c036139
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-28 14:49:58 +0530

    style

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  function clickHandler() {
-    setCount(count + 1);
-  }
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <button onClick={clickHandler}>click me</button>
-      <p>likes= {count}</p>
+      <input></input>
     </div>
   );
 }
