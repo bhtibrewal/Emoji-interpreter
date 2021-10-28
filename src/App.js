1d755ce2cd0785dee9c5a8e1491452e008707773
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-28 17:17:58 +0530

    onChange event

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from "react";
 import "./styles.css";
 import { useState } from "react";
 
+
 export default function App() {
+  var[output, setOutput]=useState("");
+
+  function inputChangeHandler(event) {
+    setOutput(event.target.value);
+    // console.log(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <input></input>
+      <input onChange={inputChangeHandler}></input>
+      welcome {output}
     </div>
   );
 }
