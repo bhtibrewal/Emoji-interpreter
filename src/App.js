30c6d4cda23989638ec1ad2a77fb913afec13ecf
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-28 14:19:08 +0530

    likes counter in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  function clickHandler() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={clickHandler}>click me</button>
+      <p>likes= {count}</p>
     </div>
   );
 }
