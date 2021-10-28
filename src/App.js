4cb34cb4db11f7c6b69b64e65cf1e492ba75f858
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-29 00:35:13 +0530

    app working

--- src/App.js
+++ src/App.js
@@ -2,20 +2,65 @@ import React from "react";
 import "./styles.css";
 import { useState } from "react";
 
+const emojiDictionary = {
+  "😂": "joy",
+  "😇": "innocent",
+  "😉": "wink",
+  "😪": "sleepy",
+  "🙄": "roll eyes",
+  "🥳": "partying"
+};
+var emojis = Object.keys(emojiDictionary);
+//console.log(emojis);
 
 export default function App() {
-  var[output, setOutput]=useState("");
-
+  const [output, setOutput] = useState("");
   function inputChangeHandler(event) {
-    setOutput(event.target.value);
-    // console.log(event.target.value);
+    var input = event.target.value;
+    var meaning = "Meaning: " + emojiDictionary[input];
+
+    if (meaning === undefined) meaning = "we don't have that in our database";
+
+    setOutput(meaning);
+  }
+  function eventHandler(item) {
+    var opt = "meaning: " + emojiDictionary[item];
+    console.log(opt);
   }
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Emoji Interpreter</h1>
+
       <input onChange={inputChangeHandler}></input>
-      welcome {output}
+      <h3>{output}</h3>
+
+      <ul style={{ display: "flex", listStyle: "none" }}>
+        {emojis.map(function (item) {
+          return (
+            <li
+              style={{ fontSize: "2rem", padding: "0.7rem" }}
+              key={item}
+              onMouseOver={() => eventHandler(item)}
+              
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+<br>
+      {emojis.map(function (emojis) {
+        return (
+          <span 
+          style={{ fontSize: "2rem", padding:".5rem" }} 
+          key={emojis}
+          onMouseOver={() => eventHandler(emojis)}
+            >
+            {emojis}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
