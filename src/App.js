e3e13f80ff8cf28c4e7956c4de4f56d55035c938
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-29 12:52:40 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -14,28 +14,33 @@ var emojis = Object.keys(emojiDictionary);
 //console.log(emojis);
 
 export default function App() {
-  const [output, setOutput] = useState("");
+  const [output, setOutput] = useState(" ");
+  const [meaning, setMeaning] = useState(" ");
+
   function inputChangeHandler(event) {
     var input = event.target.value;
-    var meaning = "Meaning: " + emojiDictionary[input];
+    var mean = "Meaning: " + emojiDictionary[input];
 
-    if (meaning === undefined) meaning = "we don't have that in our database";
+    if (mean === undefined) mean = "we don't have that in our database";
 
-    setOutput(meaning);
+    setMeaning(mean);
   }
   function eventHandler(item) {
     var opt = "meaning: " + emojiDictionary[item];
-    console.log(opt);
+    setOutput(opt);
   }
 
   return (
     <div className="App">
-      <h1>Emoji Interpreter</h1>
+      <h1 className="text">Emoji Interpreter</h1>
 
-      <input onChange={inputChangeHandler}></input>
-      <h3>{output}</h3>
+      <input
+        placeholder="enter emoji here"
+        onChange={inputChangeHandler}
+      ></input>
+      <h3>{meaning}</h3>
 
-      <ul style={{ display: "flex", listStyle: "none" }}>
+      {/* <ul style={{ display: "flex", listStyle: "none" }}>
         {emojis.map(function (item) {
           return (
             <li
@@ -48,19 +53,21 @@ export default function App() {
             </li>
           );
         })}
-      </ul>
-<br>
+      </ul> */}
+      <br />
       {emojis.map(function (emojis) {
         return (
-          <span 
-          style={{ fontSize: "2rem", padding:".5rem" }} 
-          key={emojis}
-          onMouseOver={() => eventHandler(emojis)}
-            >
+          <span
+            style={{ fontSize: "2rem", padding: ".5rem" }}
+            key={emojis}
+            onMouseOver={() => eventHandler(emojis)}
+          >
             {emojis}{" "}
           </span>
         );
       })}
+      <br />
+      <h3 className="text"> {output} </h3>
     </div>
   );
 }
